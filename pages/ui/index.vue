e395b3d99c3d6b3a16242d247091c7c9b9c821ac
mars.huang <mars.huang@d1m.cn>
2020-04-03 16:38:03 +0800

    [ui] ui 更新

--- pages/ui/index.vue
+++ pages/ui/index.vue
@@ -1,57 +1,148 @@
 <template>
   <div class="ui-guide">
-    <div @click="loading">loading 加载</div>
-    <div @click="toast">toast 提示</div>
-    <div class="scroll-text">
-      <mv-scrollbar>
-        <ul>
-          <li>1</li>
-          <li>2</li>
-          <li>4</li>
-          <li>1</li>
-          <li>2</li>
-          <li>4</li>
-          <li>1</li>
-          <li>2</li>
-          <li>4</li>
-        </ul>
-      </mv-scrollbar>
+    <h1>基础 UI组件</h1>
+    <div class="block">
+      <h2>Loading 加载</h2>
+      <mv-button
+              @click.native="loading"
+              :type-style="button.typeStyle"
+      >
+        加载
+      </mv-button>
     </div>
-    <mv-select v-model="addressModel" placeholder="请选择">
-      <mv-option
-        v-for="(item, Itemindex) in address.region"
-        :key="Itemindex"
-        :label="item.name"
-        :value="item.code"
-      ></mv-option>
-    </mv-select>
-    <mv-button-group>
-      <mv-button :type-style="button.typeStyle">按钮1</mv-button>
-      <mv-button :type-style="button.typeStyle">按钮2</mv-button>
-      <mv-button :type-style="button.typeStyle">按钮3</mv-button>
-    </mv-button-group>
-    <div class="mv-dialog" @click="mvDialog">dialog 对话框</div>
+    <div class="block">
+      <h2>Toast 提示框</h2>
+      <mv-button
+              @click.native="toast"
+              :type-style="button.typeStyle"
+      >
+        提示框
+      </mv-button>
+    </div>
+    <div class="block">
+      <h2>Scroll Bar 滚动条</h2>
+      <div class="scroll-text">
+        <mv-scrollbar :complete="scrollBar">
+          <ul>
+            <li v-for="(item, index) in 50" :key="index">滚动内容{{item}}</li>
+          </ul>
+        </mv-scrollbar>
+      </div>
+    </div>
+    <div class="block">
+      <h2>Select 三级联动</h2>
+      <div class="select-wrap">
+        <mv-select
+                v-model="regionModel"
+                :placeholder="address.region.placeholder"
+                :disabled="address.region.disabled"
+                :scroll-bar="scrollBar"
+        >
+          <mv-option
+                  v-for="(item, Itemindex) in address.region.options"
+                  :key="Itemindex"
+                  :label="item.name"
+                  :value="item.code"
+          ></mv-option>
+        </mv-select>
+        <mv-select
+                v-model="cityModel"
+                :placeholder="address.city.placeholder"
+                :disabled="address.city.disabled"
+                :scroll-bar="scrollBar"
+        >
+          <mv-option
+                  v-for="(item, Itemindex) in address.city.options"
+                  :key="Itemindex"
+                  :label="item.name"
+                  :value="item.code"
+          ></mv-option>
+        </mv-select>
+        <mv-select
+                v-model="districtModel"
+                :placeholder="address.district.placeholder"
+                :disabled="address.district.disabled"
+                :scroll-bar="scrollBar"
+        >
+          <mv-option
+                  v-for="(item, Itemindex) in address.district.options"
+                  :key="Itemindex"
+                  :label="item.name"
+                  :value="item.code"
+          ></mv-option>
+        </mv-select>
+      </div>
+    </div>
+    <div class="block">
+      <h2>Button 按钮</h2>
+      <mv-button :type-style="button.typeStyle">按钮</mv-button>
+    </div>
+    <div class="block">
+      <h2>Button Group 组合按钮</h2>
+      <mv-button-group>
+        <mv-button :type-style="button.typeStyle">按钮1</mv-button>
+        <mv-button :type-style="button.typeStyle">按钮2</mv-button>
+        <mv-button :type-style="button.typeStyle">按钮3</mv-button>
+      </mv-button-group>
+    </div>
+    <div class="block">
+      <h2>Dialog 对话框</h2>
+      <mv-button
+              @click.native="mvDialog"
+              :type-style="button.typeStyle"
+      >
+        对话框
+      </mv-button>
+    </div>
+    <div class="block">
+      <h2>Message Box 信息弹窗</h2>
+      <mv-button
+              @click.native="mvMessageBox"
+              :type-style="button.typeStyle"
+      >
+        信息弹窗
+      </mv-button>
+    </div>
+    <div class="block">
+      <h2>Pagination 分页</h2>
+      <mv-pagination
+              :page-count="paginate.count"
+              :page-range="paginate.pageRange"
+              :current-page="paginate.currentPage"
+              :page-margin="paginate.pageMargin"
+              :prev-text="paginate.prevText"
+              :next-text="paginate.nextText"
+      >
+      </mv-pagination>
+      <mv-pagination
+              :page-count="paginate.count"
+              :page-range="paginate.pageRange"
+              :current-page="paginate.currentPage"
+              :page-margin="paginate.pageMargin"
+      >
+      </mv-pagination>
+    </div>
     <mv-dialog
-      :visible.sync="dialogShow"
-      :position="`center`"
-      :before-close="handleBeforeClose"
-      title="提示"
-      width="30%"
-      @closed="handleClosed"
+            :visible.sync="dialogShow"
+            :position="`center`"
+            :before-close="handleBeforeClose"
+            title="提示"
+            width="30%"
+            @closed="handleClosed"
     >
       <div class="dialog-content">
         dialog 组件
       </div>
       <div slot="footer" class="dialog-footer-content">
         <mv-button
-          @btnClick="handleBtnClick"
+                @btnClick="handleBtnClick"
         >
           取消
         </mv-button>
         <mv-button
-          :type="button.type"
-          :type-style="button.typeStyle"
-          @btnClick="handleBtnClick"
+                :type="button.type"
+                :type-style="button.typeStyle"
+                @btnClick="handleBtnClick"
         >
           确定
         </mv-button>
@@ -61,85 +152,189 @@
 </template>
 
 <script>
-import Vue from 'vue'
-import address from '@/assets/json/address'
-import {
-  Button,
-  ButtonGroup,
-  Dialog,
-  Select,
-  Scrollbar
-} from '@/components/ui/index'
-Vue.use(ButtonGroup)
-Vue.use(Button)
-Vue.use(Dialog)
-Vue.use(Select)
-Vue.use(Scrollbar)
-export default {
-  data () {
-    return {
-      dialogShow: false,
-      address: {
-        region: {},
-        city: {},
-        district: {}
-      },
-      addressModel: 'CN-2',
-      button: {
-        type: 'submit',
-        typeStyle: 'primary'
-      }
-    }
-  },
-  created () {
-    const allAddress = address.data
-    for (const key in allAddress) {
-      for (const i in allAddress[key]) {
-        this.address[key][i] = allAddress[key][i][i]
-      }
-    }
-  },
-  methods: {
-    loading () {
-      const loading = this.$loading()
-      setTimeout(() => { loading.close() }, 2000)
-    },
-    toast () {
-      this.$toast({
-        visible: true,
-        duration: 2000,
-        message: '加入购物成功'
-      })
-    },
-    mvDialog () {
-      this.dialogShow = true
-    },
-    handleBeforeClose (callback) {
-      setTimeout(() => {
-        const flag = true
-        if (callback) {
-          callback(flag)
+  import Vue from 'vue'
+  import address from '@/assets/json/address'
+  import {
+    Button,
+    ButtonGroup,
+    Dialog,
+    Select,
+    Scrollbar,
+    Pagination
+  } from '@/components/ui/index'
+  Vue.use(ButtonGroup)
+  Vue.use(Button)
+  Vue.use(Dialog)
+  Vue.use(Select)
+  Vue.use(Scrollbar)
+  Vue.use(Pagination)
+  export default {
+    data () {
+      return {
+        scrollBar: true,
+        dialogShow: false,
+        address: {
+          region: {
+            placeholder: '请选择省',
+            disabled: false,
+            options: {}
+          },
+          city: {
+            placeholder: '请选择市',
+            disabled: true,
+            options: {}
+          },
+          district: {
+            placeholder: '请选择区',
+            disabled: true,
+            options: {}
+          }
+        },
+        regionModel: 'CN-5',
+        cityModel: '',
+        districtModel: '',
+        button: {
+          type: 'submit',
+          typeStyle: 'primary'
+        },
+        paginate: {
+          count: 20, // 总页数
+          pageRange: 5, // 页码折叠区间数量
+          currentPage: 6, // 当前页码
+          pageMargin: 2, // 折叠前后出现几个页码按钮
+          prevText: '上一页',
+          nextText: '下一页'
         }
-      }, 1000)
+      }
     },
-    handleClosed () {
-      alert('dialog关闭后执行的事件')
+    watch: {
+      regionModel(newVal) {
+        const allAddress = address.data
+        this.cityModel = ''
+        this.districtModel = ''
+        this.address.city.disabled = false
+        for (const i in allAddress.region) {
+          for (const key in allAddress.region[i]) {
+            if (allAddress.region[i][key].code === newVal) {
+              this.address.city.options = allAddress.city[key]
+            }
+          }
+        }
+      },
+      cityModel(newVal) {
+        const allAddress = address.data
+        this.districtModel = ''
+        this.address.district.disabled = false
+        for (const i in allAddress.district) {
+          for (const key in allAddress.district[i]) {
+            if (allAddress.district[i][key].code === newVal) {
+              this.address.district.options = allAddress.district[key]
+            }
+          }
+        }
+      }
     },
-    handleBtnClick () {
-      console.log('button 点击事件')
-      this.dialogShow = false
+    created () {
+      const allAddress = address.data
+      for (const key in allAddress.region) {
+        Object.assign(this.address.region.options, allAddress.region[key])
+      }
+    },
+    methods: {
+      loading () {
+        const loading = this.$loading({
+          spinner: 'mv-loading',
+          text: '加载中文字更改'
+        })
+        setTimeout(() => { loading.close() }, 2000)
+      },
+      toast () {
+        this.$toast({
+          visible: true,
+          duration: 2000,
+          message: '加入购物成功'
+        })
+      },
+      mvDialog () {
+        this.dialogShow = true
+      },
+      mvMessageBox () {
+        this.$messageBox({
+          type: 'confirm',
+          zIndex: 1002,
+          title: '提示',
+          content: '是否删除？',
+          cancelButtonText: '取消',
+          confirmButtonText: '确定'
+        }).then(() => {
+          setTimeout(() => {
+            alert('reslove')
+          },1000)
+        }).catch(() => {
+          setTimeout(() => {
+            alert('reject')
+          },1000)
+        })
+      },
+      handleBeforeClose (callback) {
+        setTimeout(() => {
+          const flag = true
+          if (callback) {
+            callback(flag)
+          }
+        }, 1000)
+      },
+      handleClosed () {
+        alert('dialog关闭后执行的事件')
+      },
+      handleBtnClick () {
+        this.dialogShow = false
+      }
     }
   }
-}
 </script>
 
 <style lang="scss" scoped>
   .ui-guide {
-    padding: 100px;
+    padding: 50px 100px;
+
+    h1 {
+      margin-bottom: 50px;
+    }
+
+    .block {
+      margin-bottom: 30px;
+
+      &:last-child {
+        margin-bottom: 0;
+      }
+    }
+
+    .select-wrap {
+      display: flex;
+
+      .mv-select {
+        margin-right: 15px;
+      }
+    }
 
     .scroll-text {
-      height: 50px;
+      height: 250px;
       overflow: hidden;
+      width: 240px;
+
+      li {
+        height: 34px;
+        line-height: 34px;
+      }
+    }
+
+    .mv-pagination {
+      margin-bottom: 15px;
+
+      &:last-child {
+        margin-bottom: 0;
+      }
     }
   }
 </style>
